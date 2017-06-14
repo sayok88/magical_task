23bb1329ee1d1b357be8d9c90a1c0f00e6adc1d2
Sayok Bhattacharya <namesake88@gmail.com>
2017-06-14 11:33:46 +0530

    commit2

--- magical_task/settings.py
+++ magical_task/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(#b%!4l@-z34e8!-bzz^+qp^pf=d4f%nv!39v_5tfq)^ock#z&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost','0.0.0.0']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'apps.app1'
 ]
